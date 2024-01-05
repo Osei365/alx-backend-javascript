34f225d282c383b27b5285c122c06b723f1ea1bc
Osei <williamsabodunrin@gmail.com>
2024-01-05 01:23:32 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,24 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     } else {
       this._length = length;
     }
-    this._students = students;
+    
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    } else if (!students.every((i) => typeof i === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    } else {
+      this._students = students; 
+    }
   }
 
   get name() {
