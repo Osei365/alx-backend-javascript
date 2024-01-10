bf9689d56249e8632d469373c5c1046ecf51cbf6
Osei <williamsabodunrin@gmail.com>
2024-01-10 01:47:15 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,8 @@
 export default function updateStudentGradeByCity(students, city, grades) {
   return students.filter((student) => student.location === city).map((element) => {
-    const score = grades.filter((grade) => grade.studentId === element.id);
-    if (score.length > 0) {
-      element['grade'] = score[0].grade;
-    } else {
-      element['grade'] = 'N/A';
-    }
+    const score = grades
+      .filter((grade) => grade.studentId === element.id)
+      .map((el) => el.grade)[0];
+    element['grade'] = score || 'N/A';
   });
 }
