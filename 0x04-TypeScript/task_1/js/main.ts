3c1021c99be37892e9a643743b406b90031bfea8
Osei <williamsabodunrin@gmail.com>
2024-01-11 01:24:28 +0000

    main work

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,5 +5,32 @@ interface Teacher {
     yearsOfExperience?: number,
     location: string,
     contract?: boolean,
-    };
+};
 
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+const printTeacher = (firstName:string, lastName:string) : string => `${firstName.charAt(0)}. ${lastName}`;
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework = () : string => 'Currently working';
+
+  displayName = () : string => this.firstName;
+}
+
+interface StudentConstructor {
+  (firstName: string, lastName: string): classInterface;
+}
+
+const student = new StudentClass("Paul", "jerry");
+console.log(student.displayName())
+console.log(student.workOnHomework())
