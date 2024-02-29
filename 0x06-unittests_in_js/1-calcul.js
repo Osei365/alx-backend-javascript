389ba8b5210f9ac576c933449aa0057b61fe9ead
Osei <williamsabodunrin@gmail.com>
2024-02-29 22:53:32 +0100

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,15 @@
 function calculateNumber(type, a, b){
   if (type == 'SUM') {
     return Math.round(a) + Math.round(b);
-  } else if (type === 'SUBTRACT') {
-    return Math.round(b) - Math.round(a);
-  } else if (type === 'DIVIDE') {
+  } 
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } 
+  if (type === 'DIVIDE') {
     if (Math.round(b) === 0) {
       return 'Error';
     } else {
-      return Math.round(b) / Math.round(a);
+      return Math.round(a) / Math.round(b);
     }
   }
 }
