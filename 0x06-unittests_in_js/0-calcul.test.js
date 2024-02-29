147d032b93cdbc831d81ce6f849670b2341307fa
Osei <williamsabodunrin@gmail.com>
2024-02-29 22:20:36 +0100

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,19 +1,22 @@
-const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
-describe('calculateNumber', function () {
-  it('should return 5 when added', function () {
-    assert.equal(calculateNumber(2, 3), 5);
-  });
-  it('should return 5 also', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('should return 5 also', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return 6 also', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should return 6 also', function () {
-    assert.equal(calculateNumber(1.5, 4), 6);
-  });
-});
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+    it('rounding of a', () => {
+        assert.equal(calculateNumber(15.78, 2), 18);
+        assert.equal(calculateNumber(1.2, 0), 1);
+        assert.equal(calculateNumber(3.5, 2), 6);
+    });
+
+    it('rounding of b', () => {
+        assert.equal(calculateNumber(2, 15.78), 18);
+        assert.equal(calculateNumber(0, 1.2), 1);
+        assert.equal(calculateNumber(2, 3.5), 6);
+    });
+
+    it('suming of a and b', () => {
+        assert.equal(calculateNumber(15.78, 2.5), 19);
+        assert.equal(calculateNumber(1.2, 0.2), 1);
+        assert.equal(calculateNumber(3.5, 2.7), 7);
+    });
+})
