602b50f3a341fb8ebaa64aede5960dae688108a7
Osei <williamsabodunrin@gmail.com>
2024-03-01 01:06:54 +0100

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
